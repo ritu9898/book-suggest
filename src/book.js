8a0659235ffcdf5627e3cd615b2019b1334ea3a6
ritu <ritumkapadia@gmail.com>
2021-09-11 21:37:38 +0530

    Added a loop for  rendering books

--- src/book.js
+++ src/book.js
@@ -4,16 +4,19 @@ import React from 'react';
 class Book extends React.Component {
 
   render(){
+
+    let books = [];
+
+    this.props.book.forEach((element, index) => {
+      books.push(<div className="card" style={{width: "25rem"}}><div className="card-body"><h5 className="card-title">{element[0]}</h5><h6 className="card-subtitle mb-2 text-muted">{element[1]}</h6><p className="card-text"></p></div>
+    </div>);
+    console.log(books);
+    });
     for (var i = 0; i < this.props.book.length; i++) {
-      console.log('i: '+i);
       return(
-        <div className="card" style={{width: "25rem"}}>
-          <div className="card-body">
-            <h5 className="card-title">{this.props.book[i][0]}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">{this.props.book[i][1]}</h6>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
+       <div>
+        {books}
+       </div>
       );
     }
   }
